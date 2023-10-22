aee18689827804c531eafc3da9fab343f5d02b79
Sara Garcia <saragca81@gmail.com>
2023-10-22 12:44:06 -0700

    updated a few routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 // "/notes" respondes with the notes.html file
 router.get('/notes',(req,res) => 
-    res.sendFile(path.join(_dirname, '/notes.html'))
+    res.sendFile(path.join(_dirname, '../notes.html'))
 );
 
 // All other routes respond to the index.html file
 router.get('*',(req,res) => 
-    res.sendFile(path.join(_dirname, '/index.html'))
+    res.sendFile(path.join(_dirname, '../index.html'))
 );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes')
-const hmtlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 //Initialize the app and create a port
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
-app.use('/', hmtlRoutes);
+app.use('/', htmlRoutes);
 
 
 //Start the server on the port
